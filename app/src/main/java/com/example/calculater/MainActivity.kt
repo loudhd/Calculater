18b976ca33718657b138a25a7ae40b32dc75608c
acer <wongsatorn1501003@gmail.com>
2019-10-16 20:14:43 +0700

    12

--- app/src/main/java/com/example/calculater/MainActivity.kt
+++ app/src/main/java/com/example/calculater/MainActivity.kt
@@ -64,9 +64,11 @@ class MainActivity : AppCompatActivity() {
 
         val opListner = View.OnClickListener { v ->
             val op = (v as Button).text.toString()
-            val value = newNumber.text.toString()
-            if (value.isNotEmpty()) {
+            try {
+                val value = newNumber.text.toString().toDouble()
                 performOperation(value, op)
+            } catch (e: NumberFormatException) {
+                newNumber.setText("")
             }
             pendingOperation = op
             displayOperation.text = pendingOperation
@@ -78,11 +80,11 @@ class MainActivity : AppCompatActivity() {
         buttonPlus.setOnClickListener(opListner)
     }
 
-    private fun performOperation(value: String, opertion: String) {
+    private fun performOperation(value: Double, opertion: String) {
         if (operand1 == null) {
-            operand1 = value.toDouble()
+            operand1 = value
         } else {
-            operand2 = value.toDouble()
+            operand2 = value
 
             if (pendingOperation == "=") {
                 pendingOperation = opertion
