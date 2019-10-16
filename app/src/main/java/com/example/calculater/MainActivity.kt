f175672e854382a4d7fbb9caf2e2b0ed618cfdac
acer <wongsatorn1501003@gmail.com>
2019-10-16 19:34:11 +0700

    5

--- app/src/main/java/com/example/calculater/MainActivity.kt
+++ app/src/main/java/com/example/calculater/MainActivity.kt
@@ -2,8 +2,18 @@ package com.example.calculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOperation by lazy{LazyThreadSafetyMode.NONE} { findViewById<TextView>(R.id.operation) }
+
+
+    private var operand1 : Double? = null
+    private var operand2:  Double = 0.0
+    private var pendingOperation = "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
